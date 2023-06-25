b8dabf61a163e7c520a6449cf8318e362467ba92
Kim Seon Woo <seonwoo960000@naver.com>
2023-06-25 20:02:29 +0900

    Set up armeria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Server server = Server.builder()
+                              .http(8080)
+                              .service("/hello", (ctx, req) -> HttpResponse.of("hello world"))
+                              .build();
+
+        server.closeOnJvmShutdown();
+
+        server.start().join();
     }
 }
